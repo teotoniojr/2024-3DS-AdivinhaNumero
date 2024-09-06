48bba00a3773f3fed83a8f3833bce69c8d2023b7
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2024-09-06 10:39:37 -0300

    Add files via upload

---
+++ reconhecimentoVoz.js
@@ -0,0 +1,22 @@
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const elementoChute = document.getElementById('chute')
+ 
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e){
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+}
+
+function exibeChuteNaTela(chute){
+      elementoChute.innerHTML = `
+      <div>Você disse:</div>
+      <span class="box"> ${chute} </span>
+      `
+}
